d2ffb19b3ff37cdefbbd36cb43ca7e491b991bd2
TheGamingMahi <137300915+TheGamingMahi@users.noreply.github.com>
2023-07-28 12:52:14 +0600

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,16 +1,36 @@
-local plugin = lukkit.plugin("NoSleep", "1.0")
---local storage = plugin.getconfig()
+local pluginConfig = plugin.getStorageObject("config.yml") -- Create config.yml
+
+if pluginConfig:setDefaultValue("Kill-the-player-On-Sleep", false) then -- Create value
+  pluginConfig:save() -- Save value
+end
+
+if pluginConfig:setDefaultValue("Enable-Sleep-Message", true) then -- Create value
+  pluginConfig:save() -- Save value
+end
+
+if pluginConfig:setDefaultValue("On-Sleep-Message", "Sleeping not allowed in this server") then -- Create value
+  pluginConfig:save() -- Save value
+end
+
+local PlayerKill = pluginConfig:getValue("Kill-the-player-On-Sleep")
+local SleepMessage = pluginConfig:getValue("On-Sleep-Message")
+local EnableSleepMessage = pluginConfig:getValue("Enable-Sleep-Message")
 
 plugin.onEnable(function()
-    logger.info("NoSleep plugin enabled!")
+  logger.info("NoSleep plugin enabled!")
 end)
 
 plugin.onDisable(function()
-    logger.info("NoSleep plugin disabled!")
+  logger.info("NoSleep plugin disabled!")
 end)
 
-plugin.registerevent("PlayerBedEnterEvent", function(event)
-    local player = event:getplayer
-    player:sethealth(0)
-    Player:sendmessage("Sleeping not allowed in this server")
-  end)
+plugin.registerEvent("PlayerBedEnterEvent", function(event)
+  local player = event:getPlayer()
+  if PlayerKill then   
+    player:setHealth(0)
+  end
+  if EnableSleepMessage then
+  player:sendMessage(SleepMessage)
+  end
+  event:setCancelled(true) -- Cancel sleep
+end)
