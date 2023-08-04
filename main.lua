7d4b99b4d69af8d6028ec353c79e755ed62570a9
TheGamingMahi <137300915+TheGamingMahi@users.noreply.github.com>
2023-08-04 18:07:58 +0600

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,36 +1,38 @@
-local pluginConfig = plugin.getStorageObject("config.yml") -- Create config.yml
+-- Create a storage object with the file "config.yml"
+local pluginConfig = plugin.getStorageObject("config.yml")
 
-if pluginConfig:setDefaultValue("Kill-the-player-On-Sleep", false) then -- Create value
-  pluginConfig:save() -- Save value
-end
+-- Set default values for the configuration
+pluginConfig:setDefaultValue("Kill-the-player-On-Sleep", false)
+pluginConfig:setDefaultValue("Enable-Sleep-Message", true)
+pluginConfig:setDefaultValue("On-Sleep-Message", "Sleeping not allowed in this server")
+pluginConfig:save() -- Save default values
 
-if pluginConfig:setDefaultValue("Enable-Sleep-Message", true) then -- Create value
-  pluginConfig:save() -- Save value
-end
-
-if pluginConfig:setDefaultValue("On-Sleep-Message", "Sleeping not allowed in this server") then -- Create value
-  pluginConfig:save() -- Save value
-end
-
+-- Retrieve the configuration values
 local PlayerKill = pluginConfig:getValue("Kill-the-player-On-Sleep")
 local SleepMessage = pluginConfig:getValue("On-Sleep-Message")
 local EnableSleepMessage = pluginConfig:getValue("Enable-Sleep-Message")
 
+-- Register plugin events
 plugin.onEnable(function()
-  logger.info("NoSleep plugin enabled!")
+    logger.info("NoSleep plugin enabled!")
 end)
 
 plugin.onDisable(function()
-  logger.info("NoSleep plugin disabled!")
+    logger.info("NoSleep plugin disabled!")
 end)
 
 plugin.registerEvent("PlayerBedEnterEvent", function(event)
-  local player = event:getPlayer()
-  if PlayerKill then   
-    player:setHealth(0)
-  end
-  if EnableSleepMessage then
-  player:sendMessage(SleepMessage)
-  end
-  event:setCancelled(true) -- Cancel sleep
+    local player = event:getPlayer()
+
+    -- Check if the player should be killed on sleep
+    if PlayerKill then
+        player:setHealth(0)
+    end
+
+    -- Check if the sleep message should be sent
+    if EnableSleepMessage then
+        player:sendMessage(SleepMessage)
+    end
+
+    event:setCancelled(true) -- Cancel sleep
 end)
